60be6389b933a5dd3a68758b7a37dcc244709d2d
RyanBerta <ryanberta1234@gmail.com>
2026-06-02 17:23:34 -0700

    Added Loading/error states

--- src/Pokedex/Pokedex.jsx
+++ src/Pokedex/Pokedex.jsx
@@ -5,18 +5,30 @@ import  pokedexImg  from "../assets/poke-dex copy.webp"
 const Pokedex = () => {
     const [pokemon, setPokemon] = useState({})
     const [inputValue, setInputValue] =useState("")
+    const [isLoading, setLoading] = useState(true)
+    const [error, setError] = useState({msg:"", err: false})
     const enterBtn = document.querySelector('.start')
 
 
     const getPokemon = async(input) => {
         try{
+            setLoading(true)
             const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
             const data = await resp.json()
             
             const { name, stats, moves: abilities,  sprites: {  front_default : img }, types:[{type: {name: pokeType}}]} = data
             setPokemon({ name, stats, abilities, img, pokeType })
+            setLoading(false)
         } catch(err){
+            setLoading(false)
+            setPokemon({})
+            setError({msg:err, err:err})
+            
             console.log(`Error Message: ${err}`)
+            
+            setTimeout(() => {
+                setError({msg:"", err:false})
+            }, 3000)
         }
     }
 
@@ -68,7 +80,7 @@ if(enterBtn?.disabled === true){
       <div className="pokedex-container">
           <img src={pokedexImg} alt="Pokedex" />
           <div className="pokedex-left">
-              {pokemon.img && <img className="pokemon-sprite" src={pokemon?.img} alt="Pokemon" />}
+              {isLoading ? <div className="loading-img pokemon-sprite"></div> : pokemon.img && <img className="pokemon-sprite" src={pokemon?.img} alt="Pokemon" /> }
               <div className="search-container">
                   <input className="search" type="text" onKeyPress={handleKeyChange}onChange={handleChange} placeholder='Search for Pokemon' value={inputValue} />
                   <button onClick={handleSearch} className="start btn">Search</button><button onClick={handleClear} className="clear btn">Clear</button>
@@ -76,12 +88,13 @@ if(enterBtn?.disabled === true){
           </div>
           
         <div className="screen">
-            <div className="header">
+              {error.err && <div>The Pokemon you searched for doesn't exist, Check your spelling</div>}
+           {isLoading ? <div className="loading"></div> : <> <div className="header">
                   {pokemon.name && <p><b>Name:</b>{pokemon.name}</p>} {pokemon.pokeType && <p><b>type:</b> {pokemon.pokeType}</p>}
             </div>
-              
+                 
              {pokemon.stats && <div className="stats">
-                  <h4>Stats:</h4>
+                  <h4>Base Stats:</h4>
                   <ul className="stat-container">
                     {pokemon.stats.map(x => {
                         const {base_stat, stat: {name} } = x
@@ -98,6 +111,7 @@ if(enterBtn?.disabled === true){
                       })}
                 </ul>
               </div>}
+              </>}
         </div>
     </div>
   )
